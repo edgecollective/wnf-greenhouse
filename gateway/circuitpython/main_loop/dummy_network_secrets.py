874eb30f479f16d67911b03de7934294bfd2a325
Don Blair <donblair@gmail.com>
2020-07-16 09:31:21 -0400

    update

---
+++ gateway/circuitpython/main_loop/dummy_network_secrets.py
@@ -0,0 +1,6 @@
+network_secrets = {
+    'ssid' : b'My SSID',
+    'password' : b'My Password',
+    'farmos_pubkey' : 'efaf687b7b6cd7fefeaf6efe175254f0',
+    'farmos_privkey' : '95035885ba10e7c4577bbb0260974191'
+    }
